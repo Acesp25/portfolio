9f739d1c0d408030019b8a54b7d22abf45305fb4
Acesp25 <rii11@txstate.edu>
2026-04-01 18:08:59 -0500

    Added more visuals for tabbing and my name

--- src/utils/intro.rs
+++ src/utils/intro.rs
@@ -7,8 +7,8 @@ pub fn intro() -> Markup {
         .section-container {
             .split-layout{
                 .left-column {
-                    h1 { "Hello! Bueno! 你好!" }
-                    h2 { "I'm Aaron Espinoza" }
+                    h1 { "Hello! Hola! 你好!" }
+                    h2 { "I'm " strong { "Aaron Espinoza" } }
                     p { "I enjoy a good challenge and chocolate milk :)" }   
                     p { "My current interests include Operating Systems, Computer Networking, and Computer Vision." }
                 }      
