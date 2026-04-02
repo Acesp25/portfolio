a4f01417a8978201faa726193b0e3f7a0ffe634d
Acesp25 <rii11@txstate.edu>
2026-04-02 10:02:26 -0500

    Added popup for Experiences, I need to clean up my css

--- src/utils/experiences.rs
+++ src/utils/experiences.rs
@@ -9,39 +9,58 @@ pub fn experiences() -> Markup {
     html! {
         .experience-container {
             (experience(
+                1,
                 "FreeBSD",
                 "Google Summer of Code Student",
                 "May 2025 - Sept 2025",
-                FREEBSD_LOGO
+                FREEBSD_LOGO,
+                html! {
+                    p { "test lalalala"}
+                }
             ))
 
             (experience(
+                2,
                 "Texas State University",
                 "Deep Learning Researcher",
                 "Aug 2024 - Present",
-                TXST_LOGO
+                TXST_LOGO,
+                html! {
+                    p { "test lalalala"}
+                }
             ))
 
             (experience(
+                3,
                 "Guangdong People's Provincial Hospital",
                 "Medical AI Research Intern",
                 "May 2024 - Aug 2024",
-                GPPHOSPITAL_LOGO
+                GPPHOSPITAL_LOGO,
+                html! {
+                    p { "test lalalala"}
+                }
             ))
 
             (experience(
+                4,
                 "tekRESCUE",
                 "Maintenance Technician",
                 "Aug 2022 - May 2024",
-                TEKRESCUE_LOGO
+                TEKRESCUE_LOGO,
+                html! {
+                   p { "test lalalala"} 
+                }
             ))
         }
     }
 }
 
-fn experience(name: &str, title: &str, date: &str, logo: &str) -> Markup {
+fn experience(id: i32, name: &str, title: &str, date: &str, logo: &str, info: Markup) -> Markup {
+    let toggle_id = format!("modal-{}", id);
     html! {
-        .experience {
+        input type="checkbox" id=(toggle_id) class="modal-state";
+
+        label for=(toggle_id) class="experience" {
             .logo-container {
                 img alt=(name) src=(logo);
             }
@@ -51,6 +70,23 @@ fn experience(name: &str, title: &str, date: &str, logo: &str) -> Markup {
                     p.date { (date) }
                 }
                 p { (title) }
+            }
+        }
+
+        div class="modal-overlay" {
+            label for=(toggle_id) class="modal-backdrop" {};
+            
+            div class="modal-content" {
+                label for=(toggle_id) class="close-btn" { "x" }
+
+                .modal-header {
+                    img src=(logo) alt=(name);
+                    h3 { (title) }
+                    p.date { (date) }
+                }
+                .modal-details { 
+                    (info)
+                }
             }
         }
     }
