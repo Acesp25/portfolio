78f3ee126341a028f365822981cccd9fd1472b89
Aaron Espinoza <acesp25@freebsd.org>
2026-09-02 13:40:10 -0500

    tools: Shape tool lookup as a macro
    This allows us to only edit one place when adding a new tool. It may
    look ugly, but thats besides the point
    
    Signed-off-by: Aaron Espinoza <acesp25@freebsd.org>

--- src/comp/tools.rs
+++ src/comp/tools.rs
@@ -1,76 +1,42 @@
 use maud::{Markup, html};
 
-pub enum Tool { 
-    C, 
-    Rust, 
-    Java, 
-    Python,
-    Assembly,
-    Bash,
-    SpringBoot, 
-    MariaDB, 
-    DTrace, 
-    Jails, 
-    Bhyve, 
-    QEMU,
-    Pf, 
-    WireGuard,
-    GDB, 
-    Jira,
-    Phabricator,
-    FreeBSD,
-    OpenBSD,
-    Linux,
-    POSIX,
-    NFS,
-    Maud,
-    Rocket,
-}
+macro_rules! tools {
+    ($($variant:ident $(= $label:literal)?),* $(,)?) => {
+        pub enum Tool { $($variant),* }
 
-impl Tool {
-    fn label(&self) -> &'static str {
-        use Tool::*;
-        match &self {
-            C => "C",
-            Rust => "Rust", 
-            Java => "Java", 
-            Python => "Python",
-            Assembly => "Assembly",
-            Bash => "Bash",
-            SpringBoot => "SpringBoot", 
-            MariaDB => "MariaDB", 
-            DTrace => "DTrace", 
-            Jails => "Jails", 
-            Bhyve => "Bhyve", 
-            QEMU => "QEMU",
-            Pf => "Pf", 
-            WireGuard => "WireGuard",
-            GDB => "GDB", 
-            Jira => "Jira",
-            Phabricator => "Phabricator",
-            FreeBSD => "FreeBSD",
-            OpenBSD => "OpenBSD",
-            Linux => "Linux",
-            POSIX => "POSIX",
-            NFS => "NFS",
-            Maud => "Maud",
-            Rocket => "Rocket",
-        }
-    }
+        impl Tool {
+            fn label(&self) -> &'static str {
+                match self {
+                    $(Tool::$variant => tools!(@label $variant $(, $label)?)),*
+                }
+            }
 
-    pub fn link_tools(tools: &[Tool]) -> Markup {
-        html! {
-            @if !tools.is_empty() {
-                .tools-list {
-                    p {
-                        strong {"Tools: "}
-                        @for (i, t) in tools.iter().enumerate() {
-                            @if i > 0 { ", " }
-                                (t.label())
+            pub fn link_tools(tools: &[Tool]) -> Markup {
+                html! {
+                    @if !tools.is_empty() {
+                        .tools-list {
+                            p {
+                                strong {"Tools: "}
+                                @for (i, t) in tools.iter().enumerate() {
+                                    @if i > 0 { ", " }
+                                        (t.label())
+                                }
+                            }
                         }
                     }
                 }
             }
         }
-    }
+    };
+    (@label $variant:ident) => { stringify!($variant) };
+    (@label $variant:ident, $label:literal) => { $label };
+}
+
+tools! {
+    C, Rust, Java, Python, Assembly, Bash,
+    SpringBoot = "Spring Boot", MariaDB, PyTorch, TensorFlow, Maud, Rocket, POSIX,
+    DTrace, Jails, Bhyve = "bhyve", QEMU, Pf = "pf", GDB = "gdb",
+    Jira, Phabricator, 
+    FreeBSD, OpenBSD, Linux, 
+    NFS, WireGuard,
 }
